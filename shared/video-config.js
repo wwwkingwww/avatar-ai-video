6aebdf8da6d1cc68ac70dcbddda860a9600927d1
wwwkingwww <emarketingdream@gmail.com>
2026-05-02 11:26:59 +0800

    refactor: rename video-config -> generation-config with backward compat shim + TASK_TYPES + PHASES

---
+++ shared/video-config.js
@@ -0,0 +1,13 @@
+export {
+  TEMPLATES,
+  PLATFORMS,
+  TEMPLATE_IDS,
+  PLATFORM_IDS,
+  templateLabel,
+  platformLabel,
+  platformInfo,
+  templateList,
+  platformList,
+  templateOptions,
+  platformOptions,
+} from './generation-config.js'
